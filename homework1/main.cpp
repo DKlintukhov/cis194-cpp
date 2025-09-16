33016a77ce6723946294b64e8c1db38abffff418
Denis Klintukhov <35447261+DKlintukhov@users.noreply.github.com>
2025-09-16 09:19:58 +0300

    Add: Homework 1 (#2)

--- homework1/main.cpp
+++ homework1/main.cpp
@@ -1,8 +1,60 @@
-#include <print>
+#include <vector>
+#include <ranges>
+#include <numeric>
+#include <string>
+#include <cassert>
+#include <functional>
 
-int main(int arc, char* argv[])
+std::vector<int> to_digits(int64_t num)
 {
-    std::println("Hello world");
-    return 0;
+    std::string str = std::to_string(num);
+    return std::ranges::to<std::vector<int>>(str | std::views::transform([](char c) { return c - '0'; }));
 }
 
+auto flatten_digits() 
+{
+    return std::views::transform([](int digit)
+        {
+            if (digit < 10) {
+                return std::vector{ digit };
+            }
+            return to_digits(digit);
+        }) | std::views::join;
+}
+
+bool validate(int64_t card_number) 
+{
+    if (card_number <= 0) return false;
+
+    auto card_digits = to_digits(card_number);
+    auto reversed_digits = card_digits | std::views::reverse;
+    auto doubled_digits =
+        std::views::zip(std::views::iota(0), reversed_digits)
+        | std::views::transform([](const auto& pair) {
+            auto [i, d] = pair;
+            return i % 2 == 1 ? d * 2 : d;
+        })
+        | flatten_digits();
+
+    int sum = 0;
+    for (int digit : doubled_digits) 
+    {
+        sum += digit;
+    }
+
+    return sum % 10 == 0;
+}
+
+int main(int argc, char* argv[])
+{
+    assert(validate(0) == false);
+    assert(validate(-1) == false);
+
+    assert(validate(4012888888881881));
+    assert(!validate(4012888888881882));
+
+    assert(validate(4532015112830366));
+    assert(!validate(4532015112830367));
+    
+    return 0;
+}
